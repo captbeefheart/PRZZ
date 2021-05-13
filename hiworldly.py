20326dd9420cac4fae0add65551e3845d761a493
captbeefheart <47297178+captbeefheart@users.noreply.github.com>
2021-05-13 19:42:46 +0100

    Update hiworldly.py

--- hiworldly.py
+++ hiworldly.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'h1 check out me J3nky!'
+    return 'h1 ch3ck out me J3nky!'
 
 if __name__=="__main__":
     app.run()
