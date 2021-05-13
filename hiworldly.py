0fe6e2518c0ccf82567cb01663316c5d18f380ad
STEVV <ste@example.com>
2021-05-13 20:47:36 +0100

    jenky commit2

--- hiworldly.py
+++ hiworldly.py
@@ -3,7 +3,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'h 0000ch3ck 0ut me J3nky!!!!!!!'
+    return 'h 0000ch33ck 0ut me J3nky!!!!!!!'
 
 if __name__=="__main__":
     app.run()
